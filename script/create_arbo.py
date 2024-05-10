cc84a87e9a9c45995fd25e5954ba5b12c7114447
jrj37 <jean.raphael2810@icloud.com>
2024-05-10 15:40:49 +0200

    barre

--- script/create_arbo.py
+++ script/create_arbo.py
@@ -110,6 +110,3 @@ def collect_ids(progressbar,root,lbl):
     with open("../pickle/organism_df", 'wb') as f:
         pickle.dump(organism_df, f)
 
-with open('../pickle/organism_df.pkl', 'wb') as f:
-    pickle.dump(organism_df, f)
-
